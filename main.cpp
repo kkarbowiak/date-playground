83ba49b0d54bdef6ca5d665d2de2ded2b9e49f6a
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2022-07-01 12:54:33 +0200

    Add initial version of parsing and formatting with tz abbrev

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "date/date.h"
 #include <chrono>
+#include <string>
 #include <utility>
 #include <sstream>
 #include <stdexcept>
@@ -11,19 +12,24 @@ using sys_ms = date::sys_time<std::chrono::milliseconds>;
 using loc_ms = date::local_time<std::chrono::milliseconds>;
 using chr_m = std::chrono::minutes;
 
-auto parse(std::string const & time_point) -> std::pair<loc_ms, chr_m>
+auto parse_with_format(std::string const & time_point, std::string const & format) -> std::pair<loc_ms, chr_m>
 {
     auto is = std::istringstream(time_point);
     auto tp = loc_ms();
     auto of = chr_m();
 
-    is >> date::parse("%FT%T%Ez", tp, of);
+    is >> date::parse(format.c_str(), tp, of);
     if (is.fail())
     {
-        throw std::runtime_error("cannot parse '" + time_point + "'");
+        throw std::runtime_error("cannot parse '" + time_point + "' with format '" + format + "'");
     }
 
     return {tp, of};
+}
+
+auto parse(std::string const & time_point) -> std::pair<loc_ms, chr_m>
+{
+    return parse_with_format(time_point, "%FT%T%Ez");
 }
 
 auto format(loc_ms time_point, chr_m offset) -> std::string
@@ -39,19 +45,52 @@ auto format(loc_ms time_point, chr_m offset) -> std::string
     return os.str();
 }
 
+auto parse_abbr(std::string const & time_point) -> std::pair<loc_ms, std::string>
+{
+    auto is = std::istringstream(time_point);
+    auto tp = loc_ms();
+    auto ab = std::string();
+
+    is >> date::parse("%FT%T%Z", tp, ab);
+    if (is.fail())
+    {
+        throw std::runtime_error("cannot parse '" + time_point + "'");
+    }
+
+    return {tp, ab};
+}
+
+auto format_abbr(loc_ms time_point, std::string const & abbr) -> std::string
+{
+    auto os = std::ostringstream();
+    date::to_stream(os, "%FT%T%Z", time_point, &abbr, nullptr);
+    if (os.fail())
+    {
+        throw std::runtime_error("cannot format");
+    }
+
+    return os.str();
+}
+
 int main(int /* argc */, char * argv[])
 {
     try
     {
         auto const arg = std::string(argv[1]);
 
-        auto const [tp, of] = parse(arg);
-        auto const formatted = format(tp, of);
-
+        auto const [tp, ab] = parse_abbr(arg);
+        auto const formatted = format_abbr(tp, ab);
         std::cout
             << arg << ":\n    "
-            << tp.time_since_epoch().count() << "    " << of.count() << "\n    "
+            << tp.time_since_epoch().count() << "    " << ab << "\n    "
             << formatted << '\n';
+        // auto const [tp, of] = parse(arg);
+        // auto const formatted = format(tp, of);
+
+        // std::cout
+        //     << arg << ":\n    "
+        //     << tp.time_since_epoch().count() << "    " << of.count() << "\n    "
+        //     << formatted << '\n';
     }
     catch (std::exception const & e)
     {
